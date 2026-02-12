e54eeec6edbd30ad8136fb49c18095d544f09704
Abhishek Giri <abhishek.giri@marsintel.com>
2026-02-12 15:51:43 +0530

    all scraped data

--- helper.py
+++ helper.py
@@ -6,11 +6,11 @@ Contains the list of scraper modules to run.
 # List of scraper module names (without .py extension)
 # Each scraper should be in the 'scrapers' directory
 SCRAPERS = [
-    # "knight_commercial", e
+    # "knight_commercial",
     # "build_out",
     # "glanmor_property",
     # "glinsman_weller",
-    # "goldenberg_real_estate",
+    # "goldenberg_real_estate", 
     # "gp_surveyors",
     # 'gregory_moore_property',
     # 'gryphon_property_partners',
@@ -24,5 +24,5 @@ SCRAPERS = [
     # 'h_d_a_k',
     # 'heaney_micklethwaite',
     # 'heb_chartered_surveyors',
-    'herron_associates'
+    # 'herron_associates'
 ]
